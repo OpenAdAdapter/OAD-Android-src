a8e554733638101b08f0bdc72fa1c92220cfa990
PyraNinja <pyraninja@gmail.com>
2015-08-05 18:42:35 +0300

    deleted old projects

--- OpenAdAdapter/download/InMobi_Android_SDK_4.5.3/NativeDemoApp/README.txt
+++
@@ -1,4 +0,0 @@
-* This DemoApp is supported for tablets from Android 4.0 and above.
-* This DemoApp is supported for Smart phones from Android 2.2 and above.
-
-
--- OpenAdAdapter/gen/com/openadadapter/v1/BuildConfig.java
+++
@@ -1,9 +0,0 @@
-
-//  Copyright (c) 2015 Artem Babenko. All rights reserved.
-
-/** Automatically generated file. DO NOT MODIFY */
-package com.openadadapter.v1;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
